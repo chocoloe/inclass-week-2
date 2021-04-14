21e24bd1d16c65b1d9452a7cd02a5eba26a3fd60
Xinyuan Hu <xinyuanhu@Xinyuans-MacBook-Pro.local>
2021-04-14 18:45:15 +0800

    script file thatcomputes lightyear

---
+++ speedlight.R
@@ -0,0 +1,8 @@
+c <- 299792458
+lightMinute <- c*60
+lightHour <- lightMinute*60
+lightDay <- lightHour*24
+lightyear <- lightDay*365
+cat("length of light year:", lightyear, "\n")
+cat("that is too long")
+
